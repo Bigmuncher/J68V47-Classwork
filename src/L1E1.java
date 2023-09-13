6375bf1b1b2dfbbf1187fee45b29815d53407ab8
Ben <30083663@nescol.ac.uk>
2023-09-13 01:19:11 +0100

    Haiku

---
+++ src/L1E1.java
@@ -0,0 +1,8 @@
+public class L1E1 {
+
+    public static void main(String[] args) {
+        System.out.println("Amidst the green trees,");
+        System.out.println("Whispers of the breeze so soft,");
+        System.out.println("Nature's song takes flight.");
+    }
+}
